9b977d04c7525a160b50645c5dcc14389c6cd68d
MJayConstantino <mjconstantino12345@gmail.com>
2025-04-02 11:26:09 +0800

    added testing for get route in student api endpoint

--- backend/src/routes/__test__/student.test.ts
+++ backend/src/routes/__test__/student.test.ts
@@ -5,6 +5,7 @@ import request from "supertest";
 const route = "/students";
 
 const testStudent = {
+  id: 1,
   firstName: "John",
   lastName: "Doe",
   groupName: "Group A",
@@ -16,9 +17,10 @@ const testStudent = {
 };
 
 const wrongDataStudent = {
-  firstName: 1000, // int in the firstName and lastName
+  id: 2,
+  firstName: 1000,
   lastName: 500,
-  groupName: "Group A",
+  groupName: "Group B",
   role: "Student",
   expectedSalary: 50000,
   expectedDateOfDefense: new Date("2023-12-01"),
@@ -27,9 +29,10 @@ const wrongDataStudent = {
 };
 
 const badRequestStudent = {
-  first_name: 1000, // int in the firstName and lastName
-  last_name: 500,
-  group_name: "Group A",
+  id: 3,
+  first_name: "Jane", // int in the firstName and lastName
+  last_name: "Doe",
+  group_name: "Group C",
   role: "Student",
   expected_salary: 50000,
   expected_date: new Date("2023-12-01"),
@@ -37,21 +40,13 @@ const badRequestStudent = {
   updated_at: new Date("2023-01-01T00:00:00Z"),
 };
 
-let studentId: number;
-
 describe("The Student endpoint", () => {
-  beforeEach(async () => {
-    const student = await prisma.student.create({
-      data: testStudent,
-    });
-    studentId = student.id;
-  });
-
   afterAll(async () => {
     await prisma.student.deleteMany();
     await prisma.$disconnect();
   });
 
+  // POST ROUTE HAPPY PATH
   it("should create a new student successfully", async () => {
     const response = await request(app).post(route).send(testStudent);
 
@@ -66,6 +61,7 @@ describe("The Student endpoint", () => {
     });
   }, 20000);
 
+  // POST ROUTE SAD PATH
   it("should fail in creating a new student because of wrong data ", async () => {
     const response = await request(app).post(route).send(wrongDataStudent);
 
@@ -75,6 +71,29 @@ describe("The Student endpoint", () => {
 
   it("should fail in creating a new student because of bad data request", async () => {
     const response = await request(app).post(route).send(badRequestStudent);
+
+    expect(response.status).toBe(500);
+    expect(response.body).toBeDefined();
+  });
+
+  // GET ROUTE HAPPY PATH
+  it("should fetch all students successfully", async () => {
+    const response = await request(app).get(route);
+
+    expect(response.status).toBe(200);
+    expect(response.body).toBeDefined();
+  });
+
+  // GET ROUTE SAD PATH
+  it("should fail in fetching a because it would fetch a studnet that does not exist", async () => {
+    const response = await request(app).get(`${route}/1000`);
+
+    expect(response.status).toBe(404);
+    expect(response.body).toBeDefined();
+  });
+
+  it("should fail in fetching a because it would fetch with a bad request", async () => {
+    const response = await request(app).get(`${route}/wrongId`);
 
     expect(response.status).toBe(500);
     expect(response.body).toBeDefined();
