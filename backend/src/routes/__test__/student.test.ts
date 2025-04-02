07ec86d10f297ad0c2267cc76d5e4b2e1f8c056f
MJayConstantino <mjconstantino12345@gmail.com>
2025-04-02 11:11:43 +0800

    fixed testing for post student route

--- backend/src/routes/__test__/student.test.ts
+++ backend/src/routes/__test__/student.test.ts
@@ -2,10 +2,9 @@ import { beforeEach, describe } from "node:test";
 import { prisma, app } from "../../server";
 import request from "supertest";
 
-const route = "/api/students";
+const route = "/students";
 
 const testStudent = {
-  id: 1,
   firstName: "John",
   lastName: "Doe",
   groupName: "Group A",
@@ -17,7 +16,6 @@ const testStudent = {
 };
 
 const wrongDataStudent = {
-  id: 1,
   firstName: 1000, // int in the firstName and lastName
   lastName: 500,
   groupName: "Group A",
@@ -28,26 +26,31 @@ const wrongDataStudent = {
   updatedAt: new Date("2023-01-01T00:00:00Z"),
 };
 
-const incompleteDataStudent = {
-  firstName: "im incomplete",
+const badRequestStudent = {
+  first_name: 1000, // int in the firstName and lastName
+  last_name: 500,
+  group_name: "Group A",
+  role: "Student",
+  expected_salary: 50000,
+  expected_date: new Date("2023-12-01"),
+  created_at: new Date("2023-01-01T00:00:00Z"),
+  updated_at: new Date("2023-01-01T00:00:00Z"),
 };
 
 let studentId: number;
 
 describe("The Student endpoint", () => {
-  //   beforeEach(async () => {
+  beforeEach(async () => {
+    const student = await prisma.student.create({
+      data: testStudent,
+    });
+    studentId = student.id;
+  });
 
-  //   });
-
-  //   afterEach(async () => {
-  //     await prisma.student.deleteMany({
-  //       where: { id: studentId },
-  //     });
-  //   });
-
-  //   afterAll(async () => {
-  //     await prisma.$disconnect();
-  //   });
+  afterAll(async () => {
+    await prisma.student.deleteMany();
+    await prisma.$disconnect();
+  });
 
   it("should create a new student successfully", async () => {
     const response = await request(app).post(route).send(testStudent);
@@ -57,8 +60,11 @@ describe("The Student endpoint", () => {
     expect({
       firstName: response.body.firstName,
       lastName: response.body.lastName,
-    }).toEqual(testStudent);
-  });
+    }).toEqual({
+      firstName: testStudent.firstName,
+      lastName: testStudent.lastName,
+    });
+  }, 20000);
 
   it("should fail in creating a new student because of wrong data ", async () => {
     const response = await request(app).post(route).send(wrongDataStudent);
@@ -67,10 +73,10 @@ describe("The Student endpoint", () => {
     expect(response.body).toBeDefined();
   });
 
-  it("should fail in creating a new student because of incomplete data", async () => {
-    const response = await request(app).post(route).send(incompleteDataStudent);
+  it("should fail in creating a new student because of bad data request", async () => {
+    const response = await request(app).post(route).send(badRequestStudent);
 
-    expect(response.status).toBe(400);
+    expect(response.status).toBe(500);
     expect(response.body).toBeDefined();
   });
 });
